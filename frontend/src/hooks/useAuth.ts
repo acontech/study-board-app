0c2be3055f8f989b2a4ee3680a3cf002991483ca
kwon <korpgm@gmail.com>
2025-08-29 01:39:19 +0900

    docs: 리뷰 메세지 추가

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -2,6 +2,8 @@ import useSWR from 'swr';
 import { API_ROUTES } from '@/lib/routes';
 import fetcher from '@/lib/api';
 import { User } from '@/types/auth';
+
+// Next.js에서 **양쪽 환경(서버/클라이언트)**에서 동일한 코드로 쿠키를 다룰 수 있는 라이브러리
 import { getCookie } from 'cookies-next';
 
 const mockUser: User = {
@@ -23,7 +25,16 @@ export function useAuth() {
   );
   */
 
-  // NOTE: 임시 목업 데이터 사용
+  // REVIEW: useSWR 은 데이터 패칭 처리와 캐싱 기능을 가지고 있다.
+  //  mutate() 함수 호출을 통해서 캐시를 갱신하는 등의 기능을 실행 할 수 있다.
+
+  /*
+  | `data`      | 요청 성공 시 응답 데이터 | 화면에 보여줄 실제 데이터   |
+  | `error`     | 요청 실패 시 에러 객체   | 에러 메시지/리다이렉트 처리  |
+  | `isLoading` | 요청 중 여부            | 로딩 스피너/대기 UI     |
+  | `mutate`    | 캐시 갱신 및 재검증 함수 | 낙관적 업데이트, 강제 최신화 |
+
+  */
   const { data, error, isLoading, mutate } = useSWR<User | null>(
     API_ROUTES.ME, // 키는 유지하여 다른 곳에서 mutate 호출 시 재검증 유도
     () => {
@@ -32,6 +43,7 @@ export function useAuth() {
       return token ? mockUser : null;
     },
     {
+      // 오류 발생시 재시도 하지 않도록 설정.
       shouldRetryOnError: false,
     }
   );
